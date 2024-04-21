c543e1194f0f6f183d8f850e9fab350920102c3c
Hakan ÖZER <hakanozer02@gmail.com>
2024-04-21 12:43:03 +0300

    Ödev - 4

---
+++ Android/Days2/app/src/main/java/com/works/days_2/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.works.days_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge() // transparent
+        setContentView(R.layout.activity_main)
+
+        // sahnenin sağlı sollu padding yada buna benzer genel ayarlarını yapma fnc.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
