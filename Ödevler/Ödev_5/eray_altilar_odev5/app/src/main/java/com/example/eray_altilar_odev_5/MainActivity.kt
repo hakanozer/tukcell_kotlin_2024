1af54cec03efd604083a9b902d10d2f56851ba9b
Erayaltilar <eray.altilar@hotmail.com>
2024-05-05 15:38:26 +0300

    Eray_Altilar_Odev_5

---
+++ Ödevler/Ödev_5/eray_altilar_odev5/app/src/main/java/com/example/eray_altilar_odev_5/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.eray_altilar_odev_5
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    /// define components
+    lateinit var btnDetail: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnDetail = findViewById(R.id.btnDetail)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnDetail.setOnClickListener {
+            val i = Intent(this , DetailActivity::class.java)
+
+            startActivity(i)
+        }
+    }
+
+}
