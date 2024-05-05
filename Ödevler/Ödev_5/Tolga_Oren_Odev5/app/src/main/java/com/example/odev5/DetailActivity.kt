6e0c59cd73b5b8a3a5dc15a5e525f74b8ab25457
Tolga Ören <orentolga@hotmail.com>
2024-05-05 18:57:53 +0300

    Tolga_Oren_Odev5

---
+++ Ödevler/Ödev_5/Tolga_Oren_Odev5/app/src/main/java/com/example/odev5/DetailActivity.kt
@@ -0,0 +1,31 @@
+package com.example.odev5
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.odev5.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val arg = intent.getStringExtra("User")
+        arg?.let {
+            Log.d("user", it)
+        }
+    }
+}
---
+++ Ödevler/Ödev_5/Tolga_Oren_Odev5/app/src/main/java/com/example/odev5/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.odev5
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.odev5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.detailBtn.setOnClickListener {
+            val i = Intent(this, DetailActivity::class.java)
+            val user = binding.nameTxt.text.toString()
+            i.putExtra("User" ,user)
+            startActivity(i)
+        }
+
+    }
+}
