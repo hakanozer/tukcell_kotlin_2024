b9cfec00b2e15f3d0a47232bb965ccfbca903daf
Ömer Sungur <omersungur615@gmail.com>
2024-05-05 14:54:49 +0300

    Add HW5 -Omer Sungur

---
+++ Ödevler/Ödev_5/omer_sungur_odev_5/app/src/main/java/com/omersungur/odev_5/DetailActivity.kt
@@ -0,0 +1,15 @@
+package com.omersungur.odev_5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+    }
+}
