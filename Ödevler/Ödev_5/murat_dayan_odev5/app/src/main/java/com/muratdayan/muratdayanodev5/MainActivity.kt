b431e086e1abca4f0925ff1b6bea5a9a5ba8528a
muratdayan <muratdayan359@gmail.com>
2024-05-05 22:37:39 +0300

    murat_dayan_odev5

---
+++ Ödevler/Ödev_5/murat_dayan_odev5/app/src/main/java/com/muratdayan/muratdayanodev5/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.muratdayan.muratdayanodev5
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.muratdayan.muratdayanodev5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root ?: return)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnDetail.setOnClickListener {
+            val i =Intent(this,DetailActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
