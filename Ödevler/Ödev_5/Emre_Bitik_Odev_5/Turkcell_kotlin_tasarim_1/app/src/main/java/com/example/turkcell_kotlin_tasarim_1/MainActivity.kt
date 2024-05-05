b830d2e0e093cba0b1f3eaf78e55b5e52b0fcf35
emrebtk <54727901+emrebtk@users.noreply.github.com>
2024-05-05 22:18:04 +0300

    Emre_Bitik_Ödev_5

---
+++ Ödevler/Ödev_5/Emre_Bitik_Odev_5/Turkcell_kotlin_tasarim_1/app/src/main/java/com/example/turkcell_kotlin_tasarim_1/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.turkcell_kotlin_tasarim_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var  btnDetail: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        btnDetail = findViewById(R.id.btnDetail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        btnDetail.setOnClickListener {
+
+            val i = Intent(this,DetailActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
