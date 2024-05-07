f434ff88466d2a5385c70b064184e6f67dc7ff57
emrebtk <54727901+emrebtk@users.noreply.github.com>
2024-05-07 21:57:00 +0300

    Emre_Bitik_Vize_2

---
+++ Vize-2/Emre_Bitik_Vize_2/app/src/main/java/com/example/xmlservice/models/XmlData.kt
@@ -0,0 +1,8 @@
+package com.example.xmlservice.models
+
+data class XmlData(val Common: String,
+                   val Botanical: String,
+                   val Zone: String,
+                   val Light: String,
+                   val Price: String,
+                   val Availability:String)
