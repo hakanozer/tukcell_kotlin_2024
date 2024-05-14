6fd577315d3c15aba04a93fc8c63042566b69076
Talha Çalışır <70065773+infernotlc@users.noreply.github.com>
2024-05-14 21:59:08 +0300

    Update SharedPreferences.kt

--- Ödevler/Ödev_7/Izzet_Talha_Calisir_Odev7/SharedPreferences.kt
+++ Ödevler/Ödev_7/Izzet_Talha_Calisir_Odev7/SharedPreferences.kt
@@ -1,7 +1,4 @@
-package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class SecureSharedPreferences(context: Context) {
 
     private val sharedPreferencesName = "secure_shared_preferences"
