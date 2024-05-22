ca4847b76c06f405ed878ecc48a7ab2883a30de7
Ömer Sungur <omersungur615@gmail.com>
2024-05-22 12:13:19 +0300

    Create network module and provide Retrofit, Gson and OkHttpClient

---
+++ Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.omersungur.omer_sungur_odev_9.di
+
+import com.omersungur.omer_sungur_odev_9.core.Constants.BASE_URL
+import com.omersungur.omer_sungur_odev_9.data.remote.ProductService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.*
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ProductService {
+        return retrofit.create(ProductService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGsonConverterFactory() : GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+}
