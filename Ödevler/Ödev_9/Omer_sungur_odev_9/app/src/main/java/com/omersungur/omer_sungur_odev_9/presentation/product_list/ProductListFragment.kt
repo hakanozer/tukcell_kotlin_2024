6edc0b909ec4a93700836b0baf7748c7397b5e7a
Ömer Sungur <omersungur615@gmail.com>
2024-05-22 18:55:17 +0300

    Integrate navigation

--- Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/presentation/product_list/ProductListFragment.kt
+++ Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/presentation/product_list/ProductListFragment.kt
@@ -5,12 +5,20 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.omersungur.omer_sungur_odev_9.R
 import com.omersungur.omer_sungur_odev_9.core.Resource
+import com.omersungur.omer_sungur_odev_9.core.viewBinding
+import com.omersungur.omer_sungur_odev_9.databinding.FragmentProductListBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
-class ProductListFragment : Fragment() {
+@AndroidEntryPoint
+class ProductListFragment : Fragment(R.layout.fragment_product_list) {
 
     private val viewModel: ProductViewModel by viewModels()
+    private val binding by viewBinding(FragmentProductListBinding::bind)
+    private lateinit var adapter: ProductListAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -18,7 +26,10 @@ class ProductListFragment : Fragment() {
             viewModel.getProducts.collect {
                 when(it) {
                     is Resource.Success -> {
-                        println(it.data?.products?.size)
+                        println(it.data?.products!!.size)
+                        adapter = ProductListAdapter(it.data?.products!!)
+                        binding.rvProductList.layoutManager = LinearLayoutManager(requireContext())
+                        binding.rvProductList.adapter = adapter
                     }
                     is Resource.Error -> {
 
