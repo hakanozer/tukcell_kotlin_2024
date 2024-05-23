09e1705dc2a30363036c5a3fd5d85dc58fc2815f
Ömer Sungur <omersungur615@gmail.com>
2024-05-23 18:01:07 +0300

    Change function param for pagination

--- Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/data/remote/ProductService.kt
+++ Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/data/remote/ProductService.kt
@@ -2,9 +2,13 @@ package com.omersungur.omer_sungur_odev_9.data.remote
 
 import com.omersungur.omer_sungur_odev_9.data.remote.dto.ProductDtoResult
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ProductService {
 
-    @GET("/products")
-    suspend fun getProducts(): ProductDtoResult
+    @GET("products")
+    suspend fun getProductsWithPaging(
+        @Query("limit") limit: Int,
+        @Query("skip") skip: Int
+    ): ProductDtoResult
 }
