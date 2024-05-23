23b7dc201fb0571767f8b505f234f9987d6c7e20
Ömer Sungur <omersungur615@gmail.com>
2024-05-23 18:01:29 +0300

    Add PaginationScrollListener for pagination

---
+++ Ödevler/Ödev_9/Omer_sungur_odev_9/app/src/main/java/com/omersungur/omer_sungur_odev_9/presentation/common/PaginationScrollListener.kt
@@ -0,0 +1,26 @@
+package com.omersungur.omer_sungur_odev_9.presentation.common
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class PaginationScrollListener(private val layoutManager: LinearLayoutManager) : RecyclerView.OnScrollListener() {
+
+    abstract fun isLastPage(): Boolean
+    abstract fun isLoading(): Boolean
+
+    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+        super.onScrolled(recyclerView, dx, dy)
+
+        val visibleItemCount = layoutManager.childCount
+        val totalItemCount = layoutManager.itemCount
+        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+        if (!isLoading() && !isLastPage()) {
+            if (visibleItemCount + firstVisibleItemPosition >= totalItemCount && firstVisibleItemPosition >= 0) {
+                loadMoreItems()
+            }
+        }
+    }
+
+    abstract fun loadMoreItems()
+}
