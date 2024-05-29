dd2617ffa7ada925856abea05ac5c2524648bfb4
Ömer Sungur <omersungur615@gmail.com>
2024-05-29 16:11:11 +0300

    Change location MainActivity

--- Ödevler/Ödev_10/Omer_Sungur_odev_10/app/src/main/java/com/omersungur/omer_sungur_odev_10/presentation/MainActivity.kt
+++ Ödevler/Ödev_10/Omer_Sungur_odev_10/app/src/main/java/com/omersungur/omer_sungur_odev_10/presentation/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.omersungur.omer_sungur_odev_10
+package com.omersungur.omer_sungur_odev_10.presentation
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.omersungur.omer_sungur_odev_10.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
