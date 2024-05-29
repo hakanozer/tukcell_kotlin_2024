2b102862bcce90eec031c2775629a061bc1c77b9
Ömer Sungur <omersungur615@gmail.com>
2024-05-29 16:10:45 +0300

    Create authentication screen

---
+++ Ödevler/Ödev_10/Omer_Sungur_odev_10/app/src/main/java/com/omersungur/omer_sungur_odev_10/presentation/authentication/log_in/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.omersungur.omer_sungur_odev_10.presentation.authentication.log_in
+
+import androidx.fragment.app.Fragment
+import com.omersungur.omer_sungur_odev_10.R
+
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
