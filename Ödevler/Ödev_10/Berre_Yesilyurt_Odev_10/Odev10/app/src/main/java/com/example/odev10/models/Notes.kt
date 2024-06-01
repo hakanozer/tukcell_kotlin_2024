9f9ec29ad4bf0922f155664a1f7c92c0b778c2e1
Berre Yeşilyurt <berreyesilyurt23@gmail.com>
2024-06-01 10:17:17 +0300

    Silindi

--- Ödevler/Ödev_10/Berre_Yesilyurt_Odev_10/Odev10/app/src/main/java/com/example/odev10/models/Notes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.odev10.models
-
-data class Notes(
-    val id:Int,
-    val lessonName:String,
-    val note:Int
-)
--- Ödevler/Ödev_10/Berre_Yesilyurt_Odev_10/Odev10/app/src/main/java/com/example/odev10/models/User.kt
+++
@@ -1,7 +0,0 @@
-package com.example.odev10.models
-
-data class User(
-    val id:Int,
-    val name:String,
-    val password:String
-)
